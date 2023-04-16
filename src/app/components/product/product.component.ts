f1f8870b2e76f6a077382f4fad22aa8fb2d759bf
Gültekin <gultekinyilmaz@hotmail.com.tr>
2023-04-16 22:24:03 +0300

    18.ders 1. bölüm

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
-import { ProductResponseModel } from 'src/app/models/productResponseModel';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -23,6 +22,5 @@ export class ProductComponent implements OnInit {
       this.products = Response.data;
       this.dataLoaded = true;
     });
-
   }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { ProductResponseModel } from '../models/productResponseModel';
 import { Observable } from 'rxjs';
+
+import { Product } from '../models/product';
+import { ListResponseModel } from '../models/listResponseModel';
 
 @Injectable({
   providedIn: 'root',
@@ -11,7 +13,7 @@ export class ProductService {
 
   constructor(private hhtpClient: HttpClient) {}
 
-  getProducts():Observable<ProductResponseModel> {
-    return this.hhtpClient.get<ProductResponseModel>(this.apiUrl);
+  getProducts():Observable<ListResponseModel<Product>> {
+    return this.hhtpClient.get<ListResponseModel<Product>>(this.apiUrl);
   }
 }
